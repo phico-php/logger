8b347699dcc5620d412afa07e090a0ac9fda0b6d
indgy <65541311+indgy@users.noreply.github.com>
2024-06-29 10:53:33 +0100

    Support config()->get() syntax

--- src/functions.php
+++ src/functions.php
@@ -6,7 +6,7 @@ if (!function_exists('logger')) {
     function logger(): \Phico\Logger\Logger
     {
         static $logger;
-        $logger = ($logger) ? $logger : new \Phico\Logger\Logger(config('logger'));
+        $logger = ($logger) ? $logger : new \Phico\Logger\Logger(config()->get('logger'));
         return $logger;
     }
 }
