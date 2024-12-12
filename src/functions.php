5bcb39a6cc528d7bb22fb4a5963972cd004bd09f
indgy <65541311+indgy@users.noreply.github.com>
2024-12-12 15:48:44 +0000

    Added container @TODO

--- src/functions.php
+++ src/functions.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 if (!function_exists('logger')) {
     function logger(): \Phico\Logger\Logger
     {
+        // @TODO pull this from the container
+        // return container()->get(\Phico\Logger\Logger::class);
+
         static $logger;
         $logger = ($logger) ? $logger : new \Phico\Logger\Logger(config()->get('logger'));
         return $logger;
